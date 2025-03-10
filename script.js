4e5c95aa9085840d20eecc26308c773b0ac94819
Gunaseelan1234567890 <gunaseelangunaseelan186@gmail.com>
2025-03-10 14:46:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function playBirthdaySong() {
+    document.getElementById("birthdaySong").play();
+    alert("🎉 Wishing you a wonderful birthday filled with joy and love! 🎂🎁 Happy Birthday, bestie! 🎉 You are more than a friend; you are my sister by heart. May this year bring you endless happiness, success, and all the love you deserve. Keep shining like the star you are! ✨💖");
+}
